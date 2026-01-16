35f0707d4d62be112d9c329b9f32f2f5f1d95936
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-01-16 13:07:29 +0530

    initialize project

--- JS-Project-1/index.ts
+++ JS-Project-1/index.ts
@@ -2,32 +2,32 @@
 let base: number = 10;
 let height: number = 5;
 let area1: number = (base * height) / 2;
-console.log("Area of triangle =", area1);
+console.log("Area of triangle =", area1); //25
 
 
 // Program 2: Celsius to Fahrenheit
 let celsius: number = 25;
 let fahrenheit: number = (celsius * 9 / 5) + 32;
-console.log("Celsius to Fahrenheit =", fahrenheit);
+console.log("Celsius to Fahrenheit =", fahrenheit);  //77
 
 
 // Program 3: Area of Rectangle
 let lengt: number = 8;
 let width: number = 4;
 let area2: number = lengt * width;
-console.log("Area of Rectangle =", area2);
+console.log("Area of Rectangle =", area2);  //32
 
 
 // Program 4: Area of Circle
 let radius: number = 7;
 let area3: number = 3.14 * radius * radius;
-console.log("Area of Circle =", area3);
+console.log("Area of Circle =", area3);  //153.86
 
 
 // Program 5: Feet to Inches
 let feet: number = 6;
 let inch: number = feet * 12;
-console.log("Inches =", inch);
+console.log("Inches =", inch);  //72
 
 
 // Common variables for formulas
@@ -38,29 +38,24 @@ let c: number = 2;
 
 // Program 6: a² − b²
 let result1: number = (a - b) * (a + b);
-console.log("a^2 - b^2 =", result1);
+console.log("a^2 - b^2 =", result1);  //16
 
 
 // Program 7: (a − b)²
 let result2: number = a * a - 2 * a * b + b * b;
-console.log("(a - b)^2 =", result2);
+console.log("(a - b)^2 =", result2);  //4
 
 
 // Program 8: (a + b + c)²
-let result3: number =
-  a * a + b * b + c * c +
-  2 * a * b + 2 * b * c + 2 * a * c;
-console.log("(a + b + c)^2 =", result3);
+let result3: number = a * a + b * b + c * c + 2 * a * b + 2 * b * c + 2 * a * c;
+console.log("(a + b + c)^2 =", result3);  //100
 
 
 // Program 9: (a − b − c)²
-let result4: number =
-  a * a + b * b + c * c -
-  2 * a * b - 2 * a * c + 2 * b * c;
-console.log("(a - b - c)^2 =", result4);
+let result4: number = a * a + b * b + c * c - 2 * a * b - 2 * a * c + 2 * b * c;
+console.log("(a - b - c)^2 =", result4);  //0
 
 
 // Program 10: (a − b)³
-let result5: number =
-  a * a * a - 3 * a * a * b + 3 * a * b * b - b * b * b;
-console.log("(a - b)^3 =", result5);
+let result5: number = a * a * a - 3 * a * a * b + 3 * a * b * b - b * b * b;
+console.log("(a - b)^3 =", result5);  //8
