bc8004f70e85615da4a49c9372c55b8ca84fa69c
Devarshi Boghani <boghanidevarshi@gmail.com>
2026-01-25 12:05:54 +0530

    initialize project

--- JS-Project-2/index2.js
+++ JS-Project-2/index2.js
@@ -1,127 +1,127 @@
 // Program 1: multiplication table
-var num1 = 5;
-var i1 = 1;
-while (i1 <= 10) {
-    console.log(num1 + " x " + i1 + " = " + (num1 * i1));
-    i1++;
+var n = 5;
+var i = 1;
+while (i <= 10) {
+    console.log(n + " x " + i + " = " + (n * i));
+    i++;
 }
 // Program 2: calculate the factorial
-var num2 = 5;
+var num = 5;
 var fact = 1;
 var i2 = 1;
-while (i2 <= num2) {
+while (i2 <= num) {
     fact = fact * i2;
     i2++;
 }
-console.log("Factorial of " + num2 + " is " + fact);
+console.log("Factorial of " + num + " is " + fact);
 // Program 3: Armstrong number list
 var limit = 500;
-var num3 = 1;
+var n1 = 1;
 do {
-    var temp = num3;
+    var temp = n1;
     var sum = 0;
     do {
         var digit = temp % 10;
         sum = sum + (digit * digit * digit);
         temp = (temp - digit) / 10;
     } while (temp > 0);
-    if (sum === num3) {
-        console.log(num3);
+    if (sum === n1) {
+        console.log(n1);
     }
-    num3++;
-} while (num3 <= limit);
+    n1++;
+} while (n1 <= limit);
 // Program 4: Palindrome number list
-var limit1 = 200;
-var num4 = 1;
-while (num4 <= limit1) {
-    var temp = num4;
+var limit2 = 200;
+var n2 = 1;
+while (n2 <= limit2) {
+    var temp = n2;
     var rev = 0;
     while (temp > 0) {
         var d = temp % 10;
         rev = (rev * 10) + d;
         temp = (temp - d) / 10;
     }
-    if (rev === num4) {
-        console.log(num4);
+    if (rev === n2) {
+        console.log(n2);
     }
-    num4++;
+    n2++;
 }
 // Program 5: Fibonacci Series
-var n3 = 10; // total terms
+var count = 10; // total terms
 var a = 0;
 var b = 1;
-var i3 = 1;
-while (i3 <= n3) {
+var i5 = 1;
+while (i5 <= count) {
     console.log(a);
     var next = a + b;
     a = b;
     b = next;
-    i3++;
+    i5++;
 }
 // Program 6: Add all the integers between 0 and 30 
-var i4 = 0;
-var sum1 = 0;
+var i6 = 0;
+var sum6 = 0;
 do {
-    sum1 = sum1 + i4;
-    i4++;
-} while (i4 <= 30);
-console.log("Total sum =", sum1);
+    sum6 = sum6 + i6;
+    i6++;
+} while (i6 <= 30);
+console.log("Total sum =", sum6);
 // Program 7: 
-var i5 = 1;
+var i7 = 1;
 do {
-    if (i5 % 3 === 0 && i5 % 5 === 0) {
+    if (i7 % 3 === 0 && i7 % 5 === 0) {
         console.log("FizzBuzz");
     }
-    else if (i5 % 3 === 0) {
+    else if (i7 % 3 === 0) {
         console.log("Fizz");
     }
-    else if (i5 % 5 === 0) {
+    else if (i7 % 5 === 0) {
         console.log("Buzz");
     }
     else {
-        console.log(i5);
+        console.log(i7);
     }
-    i5++;
-} while (i5 <= 100);
+    i7++;
+} while (i7 <= 100);
 // Program 8: displays 1-2-3-4-5-6-7-8-9-10
-var output1 = "";
-for (var i5_1 = 1; i5_1 <= 10; i5_1++) {
-    output1 = output1 + i5_1;
-    if (i5_1 !== 10) {
-        output1 = output1 + "-"; // Add hyphen if it's not the last number
+var output = "";
+for (var i8 = 1; i8 <= 10; i8++) {
+    output = output + i8;
+    if (i8 !== 10) {
+        output = output + "-"; // Add hyphen if it's not the last number
     }
 }
-console.log(output1);
+console.log(output);
 // Program 9: displays 1+4+9+16+...+100 = 385
-var sum2 = 0;
+var sum9 = 0;
 var output2 = "";
-for (var i6 = 1; i6 <= 10; i6++) {
-    var square = i6 * i6;
-    sum2 = sum2 + square;
+for (var i9 = 1; i9 <= 10; i9++) {
+    var square = i9 * i9;
+    sum9 = sum9 + square;
     output2 = output2 + square;
     if (i6 !== 10) {
         output2 = output2 + "+"; // Add "+" if it's not the last number
     }
 }
-output2 = output2 + " = " + sum2; // Add the total sum at the end
+output2 = output2 + " = " + sum9; // Add the total sum at the end
 console.log(output2);
 // Program 10: display below pattern using nested for loop
-var n4 = 5;
-for (var i7 = 1; i7 <= n4; i7++) {
+var s = 5;
+for (var p = 1; p <= s; p++) {
     var row = "";
-    for (var j1 = n4; j1 > n4 - i7; j1--) {
-        row = row + j1 + "\t";
+    for (var q = s; q > s - p; q--) {
+        row = row + q + "\t";
     }
     console.log(row);
 }
 // Program 11: display below pattern using nested for loop
 var rows = 5;
-var num5 = 1;
-for (var i8 = 1; i8 <= 5; i8++) {
+var val = 1;
+for (var l = 1; l <= 5; l++) {
     var ro = "";
-    for (var j2 = 1; j2 <= i8; j2++) {
-        ro = ro + num5 + "\t";
-        num5++;
+    for (var m = 1; m <= l; m++) {
+        ro = ro + val + "\t";
+        val++;
     }
     console.log(ro);
 }
--- JS-Project-2/index2.ts
+++ JS-Project-2/index2.ts
@@ -1,33 +1,33 @@
 // Program 1: multiplication table
-let num1: number = 5;    
-let i1: number = 1;      
+let n: number = 5;    
+let i: number = 1;      
 
-while (i1 <= 10)        
+while (i <= 10)        
 {
-    console.log(num1 + " x " + i1 + " = " + (num1 * i1));
-    i1++;                
+    console.log(n + " x " + i + " = " + (n * i));
+    i++;                
 }
 
 
 // Program 2: calculate the factorial
-let num2: number = 5;          
+let num: number = 5;          
 let fact: number = 1;
 let i2: number = 1;
 
-while (i2 <= num2)
+while (i2 <= num)
 {
     fact = fact * i2;
     i2++;
 }
-console.log("Factorial of " + num2 + " is " + fact);
+console.log("Factorial of " + num + " is " + fact);
 
 
 // Program 3: Armstrong number list
 let limit: number = 500;      
-let num3: number = 1;
+let n1: number = 1;
 
 do {
-    let temp: number = num3;
+    let temp: number = n1;
     let sum: number = 0;
 
     do {
@@ -36,19 +36,19 @@ do {
         temp = (temp - digit) / 10; 
     } while (temp > 0);
 
-    if (sum === num3) {
-        console.log(num3);
+    if (sum === n1) {
+        console.log(n1);
     }
-    num3++;
-} while (num3 <= limit);
+    n1++;
+} while (n1 <= limit);
 
 
 // Program 4: Palindrome number list
-let limit1: number = 200;   
-let num4: number = 1;
+let limit2: number = 200;   
+let n2: number = 1;
 
-while (num4 <= limit1) {
-    let temp: number = num4;
+while (n2 <= limit2) {
+    let temp: number = n2;
     let rev: number = 0;
 
     while (temp > 0) {
@@ -57,82 +57,82 @@ while (num4 <= limit1) {
         temp = (temp - d) / 10;
     }
 
-    if (rev === num4) {
-        console.log(num4);
+    if (rev === n2) {
+        console.log(n2);
     }
 
-    num4++;
+    n2++;
 }
 
 
 // Program 5: Fibonacci Series
-let n3: number = 10;           // total terms
+let count: number = 10;           // total terms
 let a: number = 0;
 let b: number = 1;    
-let i3: number = 1;
+let i5: number = 1;
 
-while (i3 <= n3) {
+while (i5 <= count) {
     console.log(a);
 
     let next: number = a + b;
     a = b;
     b = next;
 
-    i3++;
+    i5++;
 }
 
 
 // Program 6: Add all the integers between 0 and 30 
-let i4: number = 0;
-let sum1: number = 0;
+let i6: number = 0;
+let sum6: number = 0;
 
 do {
-    sum1 = sum1 + i4;
-    i4++;
-} while (i4 <= 30);
+    sum6 = sum6 + i6;
+    i6++;
+} while (i6 <= 30);
 
-console.log("Total sum =", sum1);
+console.log("Total sum =", sum6);
 
 
 // Program 7: 
-let i5: number = 1;
+let i7: number = 1;
 
 do {
-    if (i5 % 3 === 0 && i5 % 5 === 0) {
+    if (i7 % 3 === 0 && i7 % 5 === 0) {
         console.log("FizzBuzz");
     }
-    else if (i5 % 3 === 0) {
+    else if (i7 % 3 === 0) {
         console.log("Fizz");
     }
-    else if (i5 % 5 === 0) {
+    else if (i7 % 5 === 0) {
         console.log("Buzz");
     }
     else {
-        console.log(i5);
+        console.log(i7);
     }
-    i5++;     
-} while (i5 <= 100);
+    i7++;     
+} while (i7 <= 100);
 
 
 // Program 8: displays 1-2-3-4-5-6-7-8-9-10
-let output1: string = "";
+let output: string = "";
 
-for(let i5: number = 1; i5 <= 10; i5++) {
-    output1 = output1 + i5;
-    if (i5 !== 10) {
-        output1 = output1 + "-";   // Add hyphen if it's not the last number
+for(let i8: number = 1; i8 <= 10; i8++) {
+    output = output + i8;
+    if (i8 !== 10) {
+        output = output + "-";   // Add hyphen if it's not the last number
     }
 }
-console.log(output1);
+console.log(output);
 
 
 // Program 9: displays 1+4+9+16+...+100 = 385
-let sum2: number = 0;
+let sum9: number = 0;
 let output2: string = "";
 
-for (let i6 = 1; i6 <= 10; i6++) {
-    let square: number = i6 * i6;
-    sum2 = sum2 + square;
+for (let i9 = 1; i9 <= 10; i9++) {
+    let square: number = i9 * i9;
+    sum9 = sum9 + square;
     output2 = output2 + square;
     
     if (i6 !== 10) {
@@ -140,20 +140,20 @@ for (let i6 = 1; i6 <= 10; i6++) {
     }
 }
 
-output2 = output2 + " = " + sum2;    // Add the total sum at the end
+output2 = output2 + " = " + sum9;    // Add the total sum at the end
 
 console.log(output2);
 
 
 // Program 10: display below pattern using nested for loop
-let n4: number = 5;     
+let s: number = 5;     
 
-for (let i7: number = 1; i7 <= n4; i7++) {
+for (let p: number = 1; p <= s; p++) {
 
     let row: string = "";
 
-    for (let j1: number = n4; j1 > n4 - i7; j1--) {
-        row = row + j1 +"\t";
+    for (let q: number = s; q > s - p; q--) {
+        row = row + q +"\t";
     }
     console.log(row);
 }
@@ -161,15 +161,15 @@ for (let i7: number = 1; i7 <= n4; i7++) {
 
 // Program 11: display below pattern using nested for loop
 let rows: number = 5;     
-let num5: number = 1;     
+let val: number = 1;     
 
-for (let i8: number = 1; i8 <= 5; i8++) {
+for (let l: number = 1; l <= 5; l++) {
 
     let ro: string = "";
 
-    for (let j2: number = 1; j2 <= i8; j2++) {
-        ro = ro + num5 + "\t";
-        num5++;
+    for (let m: number = 1; m <= l; m++) {
+        ro = ro + val + "\t";
+        val++;
     }
     console.log(ro);
 }
